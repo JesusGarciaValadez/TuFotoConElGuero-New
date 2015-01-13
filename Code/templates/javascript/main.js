d1493b32f7be8a3f90b6830837a4ad966483c7b2
alfabrad <zerovamp@hotmail.com>
2015-01-13 08:27:14 -0600

    Javascript behavior
    Some styles correctect in menu mobile
    Behavior of menu
    Contact form validation
    Share form validation

--- Code/templates/javascript/main.js
+++ Code/templates/javascript/main.js
@@ -26,10 +26,168 @@
     //  When DOM is loaded
     $( function ( ) {
 
+        //  Click behavior for Menu button in mobile version
+        $( '.cell' ).on( 'click', function( e ) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            $( '.header ul' ).toggleClass( 'active' );
+        } );
     } );
 
     //  When page is finished loaded
     $( 'document' ).ready( function ( e ) {
 
+        //  Calculate paginator's width
+        if ( $( '.pageList' ).exists() ) {
+            yourPhoto.resizePaginator();
+
+            $( window ).on( 'resize', function ( e ) {
+                e.preventDefault();
+                e.stopPropagation();
+
+                yourPhoto.resizePaginator();
+            } );
+        }
+
+        if ( $( 'form[name="contact-form"]' ).exists() ) {
+
+            //  Validation of the contact form
+            $( 'input[type="submit"]' ).on( 'click', function ( e ) {
+                e.preventDefault();
+                e.stopPropagation();
+
+                var contact         = {};
+                contact.nombre      = $.trim( $( 'input[name="name"]' ).val() );
+                contact.correo      = $.trim( $( 'input[name="email"]' ).val() );
+                contact.mensaje     = $.trim( $( 'textarea[name="comments"]' ).val() );
+                var flag            = true;
+
+                $( 'input.error, textarea.error' ).removeClass( 'error' );
+
+                //  Valida el nombre
+                if ( !yourPhoto._validateMinLength( 2, contact.nombre.length ) ) {
+                    $( 'input[name="name"]' ).addClass( 'error' );
+                    flag    = false;
+                }
+                if ( !yourPhoto._validateMaxLength( 140, contact.nombre.length ) ) {
+                    $( 'input[name="name"]' ).addClass( 'error' );
+                    flag    = false;
+                }
+
+                //  Valida el correo
+                if ( !yourPhoto._validateMinLength( 2, contact.correo.length ) ) {
+                    $( 'input[name="email"]' ).addClass( 'error' );
+                    flag    = false;
+                }
+                if ( !yourPhoto._validateMail( contact.correo ) ) {
+                    $( 'input[name="email"]' ).addClass( 'error' );
+                    flag    = false;
+                }
+
+                //  Valida que se escriba un mensaje
+                if ( !yourPhoto._validateMinLength( 8, contact.mensaje.length ) ) {
+                    $( 'textarea' ).addClass( 'error' );
+                    flag    = false;
+                }
+                if ( !yourPhoto._validateMaxLength( 140, contact.mensaje.length ) ) {
+                    $( 'textarea' ).addClass( 'error' );
+                    flag    = false;
+                }
+
+                if ( !flag ) {
+                    return false;
+                }
+
+                var errorFunction   = function () {
+
+                };
+
+                var successFunction = function ( responseText ) {
+                    //console.log(responseText.success);
+
+                    var _title, _markup;
+
+                    if ( $.parseJSON( responseText ) ) {
+
+                        responseText    = $.parseJSON( responseText );
+
+                        if( responseText && ( responseText.success === 'true' || responseText.success === true ) ) {
+
+                            alert( 'good' );
+                        } else {
+                            alert( 'bad' );
+                        }
+                    } else {
+                        alert( 'very bad' );
+                    }
+                };
+
+                yourPhoto.validateForm( 'form[name="contact-form"]', contact, errorFunction, successFunction );
+            } );
+        }
+
+        if ( $( 'form[name="share-form"]' ).exists() ) {
+
+            // Show the form to share the image.
+            $( '.social-button:last-child' ).on( 'click', 'a', function ( e ) {
+                e.preventDefault();
+                e.stopPropagation();
+
+                $( 'form[name="share-form"]' ).fadeToggle( 150 );
+            } );
+
+            //  validation of Share form
+            $( 'input[type="submit"]' ).on( 'click', function ( e ) {
+                e.preventDefault();
+                e.stopPropagation();
+
+                var share         = {};
+                share.correo      = $.trim( $( 'input[name="email"]' ).val() );
+                var flag          = true;
+
+                $( 'input.error' ).removeClass( 'error' );
+
+                //  Valida el correo
+                if ( !yourPhoto._validateMinLength( 2, share.correo.length ) ) {
+                    $( 'input[name="email"]' ).addClass( 'error' );
+                    flag    = false;
+                }
+                if ( !yourPhoto._validateMail( share.correo ) ) {
+                    $( 'input[name="email"]' ).addClass( 'error' );
+                    flag    = false;
+                }
+
+                if ( !flag ) {
+                    return false;
+                }
+
+                var errorFunction   = function () {
+
+                };
+
+                var successFunction = function ( responseText ) {
+                    //console.log(responseText.success);
+
+                    var _title, _markup;
+
+                    if ( $.parseJSON( responseText ) ) {
+
+                        responseText    = $.parseJSON( responseText );
+
+                        if( responseText && ( responseText.success === 'true' || responseText.success === true ) ) {
+
+                            $( 'form[name="share-form"]' ).fadeOut( 150 );
+                        } else {
+                            alert( 'bad' );
+                        }
+                    } else {
+                        alert( 'very bad' );
+                    }
+                };
+
+                yourPhoto.validateForm( 'form[name="share-form"]', share, errorFunction, successFunction );
+            } );
+        }
     } );
 } ) ( jQuery, window, document );
---
+++ Code/templates/javascript/yourPhoto.js
@@ -0,0 +1,229 @@
+/**
+ *
+ *  @function
+ *  @description:   Anonimous function autoexecutable
+ *  @params jQuery $.- An jQuery object instance
+ *  @params window window.- A Window object Instance
+ *  @author: @_Chucho_
+ *
+ */
+( function( $, window, undefined ) {
+
+    var _yourPhoto    = window._yourPhoto,
+
+    // Use the correct document accordingly with window argument (sandbox)
+    document    = window.document,
+    location    = window.location,
+    navigator   = window.navigator,
+    formActive;
+
+    // Map over yourPhoto in case of overwrite
+    _yourPhoto    = window.yourPhoto;
+
+    // Define a local copy of yourPhoto
+    yourPhoto = function() {
+        if ( !( this instanceof yourPhoto ) ) {
+
+            // The yourPhoto object is actually just the init constructor 'enhanced'
+            return new yourPhoto.fn.init();
+        }
+        return yourPhoto.fn.init();
+    };
+
+    //  Object prototyping
+    yourPhoto.fn = yourPhoto.prototype = {
+        /**
+         *
+         *  @function:  !constructor
+         *  @description:   Constructor method
+         *  @author: @_Chucho_
+         *
+         */
+        //  Método constructor
+        constructor:    yourPhoto,
+        /**
+         *
+         *  @function:  !init
+         *  @description:   Inicializer method
+         *  @author: @_Chucho_
+         *
+         */
+        //  !Método inicializador
+        init:   function ( ) {
+            if ( window.matchMedia( "(min-width: 400px)" ).matches ) {
+                /* the view port is at least 400 pixels wide */
+                if ( '.header nav ul li a' ) {
+                    //  Crea las instancias de jScrollPane para los elementos que simulan
+                    //  ser select tags
+                    yourPhoto.makeScrollBar( $( '.select_inventories_options .mask' ) );
+
+                    //  Trigger para emular el comportamiento de combo box
+                    $( '.selected' ).on( 'click', function ( e ) {
+
+                        e.preventDefault();
+                        e.stopPropagation();
+                        if ( $( e.currentTarget ).siblings( '.select_inventories_options' ).css( 'opacity' ) == '0' ) {
+
+                            $( e.currentTarget ).siblings( '.select_inventories_options' ).css( {
+                                display: 'none',
+                                opacity: '1',
+                                filter: ''
+                            } );
+                        }
+                        $( e.currentTarget ).siblings( '.select_inventories_options' ).slideToggle( 'fast' );
+                    } );
+
+                    $( 'body' ).on( 'click', function ( e ) {
+
+                        e.stopPropagation();
+                        $( '.select_inventories_options' ).slideUp( 'fast' );
+                    } );
+
+                    //  Almacena la opción escogida y la almacena en un input hidden
+                    $( '.jspPane ul li' ).on( 'click', function ( e ) {
+
+                        $( e.currentTarget ).parents( '.select_inventories' ).children( 'input' ).val( '' );
+
+                        var _text   = $( e.currentTarget ).text();
+                        var _rel   = $( e.currentTarget ).attr( 'rel' );
+
+                        $( e.currentTarget ).parents( '.select_inventories' ).children( 'input' ).val( _rel );
+
+                        if ( $( e.currentTarget ).parents( '#inventories_date_range_to_wrapper' ).length != 0 ) {
+
+                            yourPhoto.dateTo     = _text;
+                        } else if ( $( e.currentTarget ).parents( '#inventories_date_range_from_wrapper' ).length != 0 ) {
+
+                            yourPhoto.dateFrom   = _text;
+                        }
+
+                        $( e.currentTarget ).parents( '.select_inventories' ).children( '.selected' ).text( _text );
+
+                        $( e.currentTarget ).parents( '.select_inventories' ).children( '.selected' ).click();
+                    } );
+                }
+            } else {
+                /* the view port is less than 400 pixels wide */
+            }
+        },
+        /**
+         *
+         *  @function:  !makesUniform
+         *  @description:   Makes the uniform effect to radius and checkbox
+         *  @params jQuery selector.- A jQuery Selector
+         *  @see:   http://uniformjs.com/
+         *  @author: @_Chucho_
+         *
+         */
+        //  !Crea un efecto para poder dar estilos a los elementos checkbox,
+        //  radio, file y select
+        makesUniform:   function ( selector ) {
+            selector.uniform();
+        },
+        /**
+         *
+         *  @function:  makeScrollBar
+         *  @description:  Make jScrollPane where is needed
+         *  @params jQuery selector.- A jQuery Selector
+         *  @params Object options.- A JSON object with the options to make a
+         *                           target element a jScrollPane Element
+         *  @author: @_Chucho_
+         *  @see:   http://www.jscrollpane.kelvinluck.com/
+         *
+         */
+        //  !Crea un elemento jScrollPane.
+        makeScrollBar:  function ( selector, options ) {
+
+            var _options    = ( options == undefined ) ? {} : options;
+
+            // the element we want to apply the jScrollPane
+            selector.jScrollPane( _options );
+        },
+        /**
+         *
+         *  @function:  !closeAlert
+         *  @description:   Validación del formulario de encuesta de Marzo 2014.
+         *  @params:    Object _rules Rules for each filed of the form
+         *  @params:    Object _messages Messages for each filed of the form
+         *  @params:    Object _submitFunction Function to handle succes in petition
+         *  @params:    Object _invalidFunction Function to handle a invalid petition
+         *  @see:   http://bassistance.de/jquery-plugins/jquery-plugin-validation/ ||
+         *          http://docs.jquery.com/Plugins/Validation
+         *  @author: @_Chucho_
+         *
+         */
+        validateForm:   function ( _jSelector, _dataPass, _errorFunction, _successFunction ) {
+            var action  = $( _jSelector ).attr( 'action' );
+            $.ajax ( action, {
+                beforeSend: function ( ) {
+                    $('.error').remove();
+                    if ( $('textarea' ).val() === "" ) {
+
+                        $('textarea' ).val( 'Ninguno' );
+                    }
+                },
+                cache: false,
+                complete: function ( ) {},
+                contentType: "application/x-www-form-urlencoded",
+                converters: {
+                    "* text":       window.String,
+                    "text html":    true,
+                    "text json":    $.parseJSON,
+                    "text xml":     $.parseXML
+                },
+                data: _dataPass,
+                error:  _errorFunction,
+                success: _successFunction,
+                type: "POST"
+            } );
+        },
+        _validateMail:          function ( mail ) {
+            return ( /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test( mail ) ) ? true : false;
+        },
+        _validateNumber:        function ( numberToCheck ) {
+            return /^\d+[^a-zA-Z]+$/.test( parseInt( numberToCheck ) );
+        },
+        _validateRange:         function ( rangeTo, rangeFrom, valueToCheck ) {
+            return ( rangeTo >= valueToCheck && rangeFrom <= valueToCheck ) ? true : false;
+        },
+        _validateMinLength:     function ( minLength, valueToCheck ) {
+            return ( minLength < valueToCheck ) ? true : false;
+        },
+        _validateMaxLength:     function ( maxLength, valueToCheck ) {
+            return ( valueToCheck <= maxLength ) ? true : false;
+        },
+        _validateDate:          function ( dateToCheck ) {
+            return ( !/Invalid|NaN/.test(new Date(dateToCheck).toUTCString() ) ) ? true : false;
+        },
+        resizePaginator:    function () {
+            var _childrensLength, _childrensWidth,
+                _childrensMargin, _childrensMarginTotal,
+                _paginatorWidthPX, _paginatorWidthVW;
+            _childrensLength        = $( '.pageList li' ).length;
+            _childrensWidth         = $( '.pageList li' ).width();
+
+            //  Obtain a sample of the margin used for the items in paginator
+            //  And parse like an integer to strip px measurements
+            _childrensMargin        = parseFloat( $( '.pageList li' ).eq( 0 ).css( 'margin-right' ) );
+            //  Calculate the total of the margin used for all the items in paginator
+            _childrensMarginTotal   = _childrensMargin * ( ( _childrensLength * 2 ) - 2 );
+
+            //  Calculate the width in px
+            _paginatorWidthPX       = ( _childrensLength * _childrensWidth ) + ( _childrensMarginTotal );
+
+            //  Make the calcule to translate in viewport width
+            _paginatorWidthVW       = _paginatorWidthPX / ( window.innerWidth * 0.01 );
+            $( '.pageList' ).width( _paginatorWidthVW + 'vw' );
+        }
+    };
+
+    // Give the init function the yourPhoto prototype for later instantiation
+    yourPhoto.fn.init.prototype = yourPhoto.fn;
+
+    yourPhoto = yourPhoto.fn;
+
+    // Expose yourPhoto to the global object
+
+    window.yourPhoto  = yourPhoto;
+
+} )( jQuery, window );
