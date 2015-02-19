4e6101fb575b0377e147c6c7b7856853f05a5fb7
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2015-02-19 12:31:16 -0600

    Method for share image via email

--- Code/snippets/libs/Review.php
+++ Code/snippets/libs/Review.php
@@ -7,12 +7,14 @@ class Review extends Model {
 
     private $_PDOConn = null;
 
-    public function __construct( $conn, $db_table )  {
+    public function __construct( $conn, $db_table )
+    {
         $this->_tableName = $db_table;
         $this->_primaryKey = 'id';
         $this->setMysqlConn( $conn );
         $this->_PDOConn = $conn;
     }
+
     public function insertInit ( $info, $template, $subject, $correo, $cc = array() )
     {
 
@@ -112,6 +114,109 @@ class Review extends Model {
                 $response   = array ( 'success'=>'false', 'msg'=>'el servicio de DB no esta disponible' );
             } catch ( phpmailerException $e ) {
 
+                $this->_PDOConn->rollBack();
+                $response   = array ( 'success'=>'false', 'msg'=>'el servicio de correo no esta disponible' );
+            }
+        }
+        return $response;
+    }
+
+    public function shareImage ( $info, $template, $subject, $correo, $cc = array() )
+    {
+        //  Validación de los datos
+        $parameters = array(
+            'email' => array(
+                             'requerido' => 1,
+                             'validador' => 'esEmail',
+                             'mensaje' => utf8_encode( 'La segunda pregunta es obligatoria.' )
+                            ),
+            'image' => array(
+                             'requerido' => 1,
+                             'validador' => 'esAlfaNumerico',
+                             'mensaje' => utf8_encode( 'La imagen es obligatoria.' )
+                            ),
+        );
+
+        $form = new Validator( $info, $parameters );
+
+        // Si el formulario no es válido
+        if ( !$form->validate() )
+        {
+            return false;
+        }
+        else
+        {
+            try
+            {
+                $success    = $this->insert( $info );
+                if ( $success )
+                {
+                    $emails = explode( ',' , $correo );
+                    $to     = array();
+
+                    foreach ( $emails as $email )
+                    {
+                        $params = array(
+                            'mail' => array(
+                                            'requerido' => 1 ,
+                                            'validador' => 'esEmail',
+                                            'mensaje' => utf8_encode( 'El correo no es v&aacute;lido.' )
+                                            )
+                        );
+
+                        $destinatario = array(
+                                              'mail' => $email,
+                                              'image' => $image
+                                              );
+
+                        $form   = new Validator( $destinatario, $params );
+                        if ( ( $form->validate() ) === false )
+                        {
+                            throw new Exception('El correo ' . $email . ' no es v&aacute;lido.');
+                        }
+                        $to[] = $destinatario;
+                    }
+
+                    $vars   = array(
+                                    'email' => $info[ 'email' ],
+                                    'image' => $info[ 'image' ]
+                                    );
+
+                    $tpl    = ParserTemplate::parseTemplate( $template, $vars );
+
+                    $_cc    = $cc;
+
+                    if ( Mailer::sendMail( $subject, $tpl, $to , '' , $_cc ) )
+                    {
+                        $response   = array (
+                                             'success' => 'true',
+                                             'message' => utf8_encode( 'Muchas gracias por contestar esta encuesta.' )
+                                             );
+                    }
+                    else
+                    {
+                        $response   = array (
+                                             'success'=>'false',
+                                             'message'=>utf8_encode( 'El servicio de correo no esta disponible' )
+                                             );
+                    }
+                }
+                else
+                {
+                    $response = array(
+                                      'success' => 'false',
+                                      'message' => utf8_encode('No fue posible guardar la información.')
+                                      );
+                }
+                $this->_PDOConn->commit();
+            }
+            catch ( PDOException $e )
+            {
+                $this->_PDOConn->rollBack();
+                $response   = array ( 'success'=>'false', 'msg'=>'el servicio de DB no esta disponible' );
+            }
+            catch ( phpmailerException $e )
+            {
                 $this->_PDOConn->rollBack();
                 $response   = array ( 'success'=>'false', 'msg'=>'el servicio de correo no esta disponible' );
             }
