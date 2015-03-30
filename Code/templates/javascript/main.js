99d8a8043af9a2fb594536fe1aee3a248216e369
Jesús García <zerovamp@hotmail.com>
2015-03-30 02:38:20 +0000

    Added keyboard handler for passing between photos with arrow keys

--- Code/templates/javascript/main.js
+++ Code/templates/javascript/main.js
@@ -8,26 +8,26 @@
  *  @author: @_Chucho_
  *
  */
-(function($, window, document, undefined){
+(function($, window, document, undefined) {
     //  Revisa la disponibilidad de localStorage
     var storage;
-    if('localStorage' in window && window.localStorage !== null){
+    if ('localStorage' in window && window.localStorage !== null) {
         storage = localStorage;
     } else{
         try{
-            if(localStorage.getItem){
+            if (localStorage.getItem) {
                 storage = localStorage;
             }
-        } catch(e){
+        } catch(e) {
             storage ={};
         }
     }
 
     //  When DOM is loaded
-    $(function(){
+    $(function() {
         yourPhoto.init();
         //  Click behavior for Menu button in mobile version
-        $('.cell').on('click', function(e){
+        $('.cell').on('click', function(e) {
             e.preventDefault();
             e.stopPropagation();
 
@@ -36,34 +36,34 @@
     });
 
     //  When page is finished loaded
-    $('document').ready(function(){
+    $('document').ready(function() {
         //  Calculate paginator's width
-        if($('.pageList').exists()){
+        if ($('.pageList').exists()) {
             yourPhoto.resizePaginator();
         }
-        $(window).on('resize', function(e){
+        $(window).on('resize', function(e) {
             e.preventDefault();
             e.stopPropagation();
             //  Crea las instancias de jScrollPane para los elementos que simulan
             //  ser select tags
-            if(!$('.jspScrollable').exists()){
+            if (!$('.jspScrollable').exists()) {
                 yourPhoto.makeScrollBar($('.select_municipality .mask'));
             }
             //  Calculate paginator's width
-            if($('.pageList').exists()){
+            if ($('.pageList').exists()) {
                 yourPhoto.resizePaginator();
             }
         });
 
-        $('header form.mobile fieldset, .year-picker.mobile fieldset').on('change', 'select', function(e){
+        $('header form.mobile fieldset, .year-picker.mobile fieldset').on('change', 'select', function(e) {
             var _value  = $(e.currentTarget).val();
             window.location.href = _value;
         });
 
-        if($('form[name="contact-form"]').exists()){
+        if ($('form[name="contact-form"]').exists()) {
 
             //  Validation of the contact form
-            $('input[type="submit"]').on('click', function(e){
+            $('input[type="submit"]').on('click', function(e) {
                 e.preventDefault();
                 e.stopPropagation();
 
@@ -76,54 +76,54 @@
                 $('input.error, textarea.error').removeClass('error');
 
                 //  Valida el nombre
-                if(!yourPhoto._validateMinLength(2, contact.nombre.length)){
+                if (!yourPhoto._validateMinLength(2, contact.nombre.length)) {
                     $('input[name="name"]').addClass('error');
                     flag    = false;
                 }
-                if(!yourPhoto._validateMaxLength(140, contact.nombre.length)){
+                if (!yourPhoto._validateMaxLength(140, contact.nombre.length)) {
                     $('input[name="name"]').addClass('error');
                     flag    = false;
                 }
 
                 //  Valida el correo
-                if(!yourPhoto._validateMinLength(2, contact.correo.length)){
+                if (!yourPhoto._validateMinLength(2, contact.correo.length)) {
                     $('input[name="email"]').addClass('error');
                     flag    = false;
                 }
-                if(!yourPhoto._validateMail(contact.correo)){
+                if (!yourPhoto._validateMail(contact.correo)) {
                     $('input[name="email"]').addClass('error');
                     flag    = false;
                 }
 
                 //  Valida que se escriba un mensaje
-                if(!yourPhoto._validateMinLength(8, contact.mensaje.length)){
+                if (!yourPhoto._validateMinLength(8, contact.mensaje.length)) {
                     $('textarea').addClass('error');
                     flag    = false;
                 }
-                if(!yourPhoto._validateMaxLength(140, contact.mensaje.length)){
+                if (!yourPhoto._validateMaxLength(140, contact.mensaje.length)) {
                     $('textarea').addClass('error');
                     flag    = false;
                 }
 
-                if(!flag){
+                if (!flag) {
                     return false;
                 }
 
-                var errorFunction   = function(){
+                var errorFunction   = function() {
                     yourPhoto.showAlert('fail', 'Error al hacer el envío');
                 };
 
-                var successFunction = function(responseText){
+                var successFunction = function(responseText) {
                     //console.log(responseText.success);
 
                     var _timer, _class, _message;
 
                     try{
-                        if($.parseJSON(responseText)){
+                        if ($.parseJSON(responseText)) {
 
                             responseText    = $.parseJSON(responseText);
 
-                            if(responseText &&(responseText.success === 'true' || responseText.success === true)){
+                            if (responseText &&(responseText.success === 'true' || responseText.success === true)) {
                                 _class  = 'success';
                                 _message= 'Éxito';
                             } else{
@@ -133,12 +133,12 @@
                         } else{
                             throw new Error('No es un JSON');
                         }
-                    } catch(e){
+                    } catch(e) {
                         _class  = 'info';
                         _message= 'Intenta más tarde.';
                     } finally{
                         yourPhoto.showAlert(_class, _message);
-                        _timer  = setTimeout(function(){
+                        _timer  = setTimeout(function() {
                             yourPhoto.hideAlert(_class);
                             clearTimeout(_timer);
                         }, 5000);
@@ -149,10 +149,10 @@
             });
         }
 
-        if($('form[name="share-form"]').exists()){
+        if ($('form[name="share-form"]').exists()) {
 
             // Show the form to share the image.
-            $('.social-button:last-child').on('click', 'a', function(e){
+            $('.social-button:last-child').on('click', 'a', function(e) {
                 e.preventDefault();
                 e.stopPropagation();
 
@@ -160,7 +160,7 @@
             });
 
             //  validation of Share form
-            $('input[type="submit"]').on('click', function(e){
+            $('input[type="submit"]').on('click', function(e) {
                 e.preventDefault();
                 e.stopPropagation();
 
@@ -172,34 +172,34 @@
                 $('input.error').removeClass('error');
 
                 //  Valida el correo
-                if(!yourPhoto._validateMinLength(2, share.correo.length)){
+                if (!yourPhoto._validateMinLength(2, share.correo.length)) {
                     $('input[name="email"]').addClass('error');
                     flag    = false;
                 }
-                if(!yourPhoto._validateMail(share.correo)){
+                if (!yourPhoto._validateMail(share.correo)) {
                     $('input[name="email"]').addClass('error');
                     flag    = false;
                 }
 
-                if(!flag){
+                if (!flag) {
                     return false;
                 }
 
-                var errorFunction   = function(){
+                var errorFunction   = function() {
                     yourPhoto.showAlert('fail', 'Error al hacer el envío');
                 };
 
-                var successFunction = function(responseText){
+                var successFunction = function(responseText) {
                     //console.log(responseText.success);
 
                     var _timer, _class, _message;
 
                     try{
-                        if($.parseJSON(responseText)){
+                        if ($.parseJSON(responseText)) {
 
                             responseText    = $.parseJSON(responseText);
 
-                            if(responseText &&(responseText.success === 'true' || responseText.success === true)){
+                            if (responseText &&(responseText.success === 'true' || responseText.success === true)) {
                                 _class  = 'success';
                                 _message= 'Éxito';
                             } else{
@@ -209,12 +209,12 @@
                         } else{
                             throw new Error('No es un JSON');
                         }
-                    } catch(e){
+                    } catch(e) {
                         _class  = 'info';
                         _message= 'Intenta más tarde.';
                     } finally{
                         yourPhoto.showAlert(_class, _message);
-                        _timer  = setTimeout(function(){
+                        _timer  = setTimeout(function() {
                             yourPhoto.hideAlert(_class);
                             clearTimeout(_timer);
                         }, 5000);
@@ -226,8 +226,8 @@
         }
 
         //  Funcionamiento del botón Regresar
-        if($('.return').exists()){
-            $('section.central').on('click', '.return', function(e){
+        if ($('.return').exists()) {
+            $('section.central').on('click', '.return', function(e) {
                 e.stopPropagation();
                 e.preventDefault();
 
@@ -241,10 +241,53 @@
          *
          */
         //  !Ancla el menú cuando pasa de cierta coordenada Y al hacer scroll
-        if($('.photo aside').exists()){
-            $(window).on('scroll', function(e){
+        if ($('.photo aside').exists()) {
+            $(window).on('scroll', function(e) {
                 yourPhoto.anchorMenu('.photo aside', '30', 'share');
             });
+        }
+
+        if ( $( '.navigator' ).exists() ) {
+            window.addEventListener( "keydown", function ( e ) {
+                if ( e.defaultPrevented ) {
+                    return; // Should do nothing if the default action has been cancelled
+                }
+
+                var handled = false, _key, _url;
+                if ( e.key !== undefined ) {
+                    // Handle the event with KeyboardEvent.key and set handled true.
+                    _key    = e.key;
+                    handled = true;
+                } else if ( e.keyIdentifier !== undefined ) {
+                    // Handle the event with KeyboardEvent.keyIdentifier and set handled true.
+                    _key    = e.keyIdentifier;
+                    handled = true;
+                } else if ( e.keyCode !== undefined ) {
+                    // Handle the event with KeyboardEvent.keyCode and set handled true.
+                    _key    = e.keyCode;
+                    handled = true;
+                }
+
+                switch ( _key ) {
+                    case 'Left':
+                    case '37':
+                        _url = $('.navigator.prev li a').attr( 'href' );
+                        location.href = _url;
+                        break;
+                    case 'Right':
+                    case '39':
+                        _url = $('.navigator.next li a').attr( 'href' );
+                        location.href = _url;
+                        break;
+                    default:
+                        break;
+                }
+
+                if ( handled ) {
+                    // Suppress "double action" if event handled
+                    e.preventDefault();
+                }
+            }, true );
         }
     });
 })(jQuery, window, document);
